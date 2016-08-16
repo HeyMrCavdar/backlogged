bf484c5aaa0dae60ea85e145e6b3c6d12a05afad
Matt <matt.cavdar@gmail.com>
2016-08-16 00:10:15 -0400

    put in work on games controller, generated Game model, filled out first form.

---
+++ backlogged/app/controllers/games_controller.rb
@@ -0,0 +1,34 @@
+class GamesController < ApplicationController
+
+	def index
+	end
+
+	def show
+		@game = Game.find(params[:id])
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def create
+		@game = Game.new(game_params)
+
+		@game.save
+		redirect_to @game
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def game_params
+		params.require(:game).permit(:title, :platform, :genre, :release_date)
+	end
+
+end
