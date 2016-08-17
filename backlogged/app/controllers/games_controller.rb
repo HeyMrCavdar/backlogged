ae85621efd8dda191f952a6ceaec5a56ca3ef2bc
Matt <matt.cavdar@gmail.com>
2016-08-16 23:19:30 -0400

    Added update to games and links to edit in index and show

--- backlogged/app/controllers/games_controller.rb
+++ backlogged/app/controllers/games_controller.rb
@@ -9,19 +9,32 @@ class GamesController < ApplicationController
 	end
 
 	def new
+		@game = Game.new
 	end
 
 	def edit
+		@game = Game.find(params[:id])
 	end
 
 	def create
 		@game = Game.new(game_params)
 
-		@game.save
-		redirect_to @game
+		if @game.save
+			flash[:notice] = "Game was successfully added!"
+			redirect_to @game
+		else
+			render 'new'
+		end
 	end
 
 	def update
+		@game = Game.find(params[:id])
+
+		if @game.update(game_params)
+			redirect_to @game
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
